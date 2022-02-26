77a5c63e77a7900e428308aa0199f652d0c9b9d8
Caroline Hvirfvel <carolinehvirfvel@Carolines-MacBook-Pro.local>
2022-02-26 15:44:56 +0000

    proxy added to json to avoid CORS issues, searchbar added to container to request location

--- src/containers/WeatherContainer.js
+++ src/containers/WeatherContainer.js
@@ -1,6 +1,41 @@
+import { useEffect, useState } from "react";
+
 const WeatherContainer = () => {
+
+    const [foundLocation, setFoundLocation] = useState(null);
+    const [locationSearch, setLocationSearch] = useState(null);
+
+    useEffect(() => {
+        getLocation();
+    }, [locationSearch])
+
+    const getLocation = () => {
+        fetch(`/api/location/search/?query=${locationSearch}`)
+        .then(response => response.json())
+        .then(location => setFoundLocation(location))
+        .catch(err => console.error(err))
+    }
+
+    const handleSearchChange = (event) => {
+        setLocationSearch(event.target.value)
+    }
+
+    const handleSearchSubmit = (event) => {
+        event.preventDefault();
+
+    }
+
+    
+
     return (
-        <h1>Weather container</h1>
+        <>
+            <h1>Weather container</h1>
+            <form onSubmit={handleSearchSubmit}>
+                <input type="text" placeholder="Location" value={locationSearch} onChange={handleSearchChange}/>
+                <input type="submit" value="Search" />
+            </form>
+        </>
+
     );
 }
 
